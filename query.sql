d5d878ed4f991cdd0e3a57b5c64d17d3582414d0
Shawn15619 <shawnlu0905@gmail.com>
2017-04-20 14:23:35 +0930

    update 20 Apr

--- query.sql
+++ query.sql
@@ -50,4 +50,42 @@ FROM (
        JOIN RECHARGEMENT USING (RENTALNO)) A
     JOIN VEHICLE ON A.LICENSENO = VEHICLE.LICENSENO
     RIGHT OUTER JOIN OUTLET USING (OUTNO))
-GROUP BY OUTNO;
+GROUP BY OUTNO ORDER BY OUTNO;
+
+-- 4. List clients who have rented vehicle(s) but who have never generated any fault report. List
+-- the ID and name as well as the contact information for those clients.
+
+
+-- 5. For each outlet, calculate the number of rental agreements and the revenue generated from the
+-- rentals. Rank the outlets based on the revenue generated from the rentals. List only the top
+-- two outlets.
+
+-- 6. Generate a list of rentals (ID, Start Date, and Return Date) with the client's information
+-- (client's number and name) for the outlet with the most rentals. Also include the outlet street
+-- address.
+
+-- 7. For each client from West Virginia, list client ID and name, the number of rentals, average
+-- duration of a rental, and the number of fault reports associated with the rentals. Include “0s”
+-- for clients without any rentals.
+
+-- 8. A manager from one of the outlets wants to analyze data of the rentals for each “vehicle
+-- make” in his outlet in order to develop strategies for future purchases of vehicles and to share
+-- his findings with other outlets. Consider two different measures that would allow the
+-- manager to make the recommendations. Produce an appropriate list that includes all outlets
+-- and sort it by outlet number and vehicle make. Then, write a paragraph explaining how the
+-- measures are to be used to help making the right decisions regarding the operations of the
+-- outlets.
+
+-- 9. We need to review fault reports of cars rented in previous month. In addition to the details on
+-- each fault reports, also include information about the car on the fault report, as well as the
+-- name of employee who recorded the fault report.
+-- 10. For each outlet managers, provide the manager’s name, number of outlets he/she manages,
+-- total number of employees working at those outlets, and the total number of vehicles at those
+-- outlets.
+
+-- 11. We are interested in what types of clients we have. Based on the “web address” information,
+-- group the client by the type of client (“.edu” indicates an educational institution, “.gov” a
+-- government agency, “.org” a non-for-profit organization, and “.com” a for-profit company)
+-- and provide how many clients we have of each type/category. If we do not have a web
+-- address, then the “type” should be ‘Not Available’. For any category that has no clients
+-- currently, display a “0”.
