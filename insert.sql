2e33a03247d88c5939ce6bf74fa628b7e8e9c9c2
Shawn15619 <shawnlu0905@gmail.com>
2017-04-20 17:15:44 +0930

    update: corrected stupid mistake

--- insert.sql
+++ insert.sql
@@ -50,36 +50,42 @@ INSERT INTO OUTLET VALUES (400, '1410 NE Campus Parkway', 'Seattle','Washington'
 
 -- insert Vehicle
 
-Insert into VEHICLE values ('L1147', 'D', '1', 'WHITE', 2017, 4, 4, 683, '10-Oct-2016', '100');
+Insert into VEHICLE values ('L1147', 'A', '1', 'WHITE', 2017, 4, 4, 683, '10-Oct-2016', '100');
 Insert into VEHICLE values ('L1156', 'B', '5', 'BLACK', 2015, 4, 4, 742, '09-Oct-2016', '100');
-Insert into VEHICLE values ('L1298', 'D', '2', 'BLUE', 2016, 2, 2, 762, '13-Apr-2016', '200');
+Insert into VEHICLE values ('L1298', 'C', '2', 'BLUE', 2016, 2, 2, 762, '13-Apr-2016', '200');
 Insert into VEHICLE values ('L1259', 'A', '3', 'BLACK', 2017, 2, 4, 289, '16-Jun-2016', '200');
 Insert into VEHICLE values ('L1318', 'C', '4', 'BLACK', 2015, 4, 6, 729, '26-Jul-2016', '300');
 Insert into VEHICLE values ('L1389', 'C', '6', 'WHITE', 2016, 2, 2, 755, '27-Jun-2016', '300');
-Insert into VEHICLE values ('L1308', 'D', '7', 'WHITE', 2017, 6, 8, 374, '06-Mar-2016', '300');
-Insert into VEHICLE values ('L1408', 'D', '8', 'SILVER GRAY', 2015, 2, 2, 370, '12-Mar-2016', '400');
+Insert into VEHICLE values ('L1308', 'A', '7', 'WHITE', 2017, 6, 8, 374, '06-Mar-2016', '300');
+Insert into VEHICLE values ('L1408', 'B', '8', 'SILVER GRAY', 2015, 2, 2, 370, '12-Mar-2016', '400');
 Insert into VEHICLE values ('L1472', 'A', '9', 'SILVER GRAY', 2016, 6, 8, 361, '20-Mar-2016', '400');
 Insert into VEHICLE values ('L1485', 'A', '10', 'WHITE', 2017, 2, 2, 238, '05-Aug-2016', '400');
+Insert into VEHICLE values ('L1487', 'A', '17', 'BLUE', 2017, 4, 4, 200, '05-Apr-2017', '400');
 
--- insert rechargement
+-- insert RAGREEMENT
 
-Insert into RECHARGEMENT values ('R1002', '12-Feb-2017', '14-Feb-2017', 2232, 2789, 'Third Party Fire & Theft', 'c3011', 'L1156');
-Insert into RECHARGEMENT values ('R1003', '04-Mar-2017', '07-Mar-2017', 6646, 7322, 'Comprehensive', 'c3005', 'L1147');
-Insert into RECHARGEMENT values ('R1004', '21-Mar-2017', '29-Mar-2017', 2790, 2960, 'Third Party Property Damage', 'c3013', 'L1156');
-Insert into RECHARGEMENT values ('R2005', '12-Mar-2017', '16-Mar-2017', 331, 1041, 'Comprehensive', 'c3005', 'L1259');
-Insert into RECHARGEMENT values ('R2006', '18-Feb-2017', '25-Mar-2017', 2420, 2605, 'Third Party Fire & Theft', 'c3014', 'L1298');
-Insert into RECHARGEMENT values ('R2007', '29-Mar-2017', '04-Apr-2017', 2605, 4761, 'Comprehensive', 'c3015', 'L1298');
-Insert into RECHARGEMENT values ('R2008', '28-Mar-2017', '08-Apr-2017', 1041, 1968, 'Third Party Fire & Theft', 'c3011', 'L1259');
-Insert into RECHARGEMENT values ('R3009', '11-Jan-2017', '07-Mar-2017', 9374, 10437, 'Comprehensive', 'c3002', 'L1308');
-Insert into RECHARGEMENT values ('R3010', '07-Mar-2017', '02-Apr-2017', 8740, 9533, 'Third Party Property Damage', 'c3003', 'L1318');
-Insert into RECHARGEMENT values ('R3011', '16-Feb-2017', '26-Mar-2017', 6158, 7324, 'Comprehensive', 'c3012', 'L1318');
-Insert into RECHARGEMENT values ('R3012', '26-Feb-2017', '08-Mar-2017', 428, 1247, 'Third Party Property Damage', 'c3004', 'L1389');
-Insert into RECHARGEMENT values ('R4013', '27-Mar-2017', '10-Mar-2017', 3880, 4506, 'Third Party Fire & Theft', 'c3003', 'L1485');
-Insert into RECHARGEMENT values ('R4014', '30-Mar-2017', '15-Mar-2017', 3552, 4175, 'Comprehensive', 'c3010', 'L1472');
-Insert into RECHARGEMENT values ('R4015', '18-Mar-2017', '22-Feb-2017', 1473, 1632, 'Third Party Property Damage', 'c3012', 'L1408');
-Insert into RECHARGEMENT values ('R4016', '07-Mar-2017', '10-Mar-2017', 1634, 2000, 'Third Party Property Damage', 'c3001', 'L1408');
-Insert into RECHARGEMENT values ('R4017', '28-Mar-2017', '01-Apr-2017', 4507, 4800, 'Third Party Fire & Theft', 'c3008', 'L1485');
-Insert into RECHARGEMENT values ('R4018', '03-Apr-2017', '07-Apr-2017', 2001, 2221, 'Third Party Fire & Theft', 'c3007', 'L1408');
+Insert into RAGREEMENT values ('R1002', '12-Feb-2017', '14-Feb-2017', 2232, 2789, 'Third Party Fire & Theft', 'c3011', 'L1156');
+Insert into RAGREEMENT values ('R1003', '04-Mar-2017', '07-Mar-2017', 6646, 7322, 'Comprehensive', 'c3005', 'L1147');
+Insert into RAGREEMENT values ('R1004', '21-Mar-2017', '29-Mar-2017', 2790, 2960, 'Third Party Property Damage', 'c3013', 'L1156');
+Insert into RAGREEMENT values ('R1005', '01-Apr-2017', '14-Apr-2017', 2789, 3002, 'Comprehensive', 'c3001', 'L1156');
+Insert into RAGREEMENT values ('R1006', '04-Apr-2017', '07-Mar-2017', 7330, 7722, 'Comprehensive', 'c3005', 'L1147');
+Insert into RAGREEMENT values ('R1007', '21-Apr-2017', '29-Apr-2017', 2960, 3900, 'Third Party Property Damage', 'c3013', 'L1156');
+Insert into RAGREEMENT values ('R2005', '12-Mar-2017', '16-Mar-2017', 331, 1041, 'Comprehensive', 'c3005', 'L1259');
+Insert into RAGREEMENT values ('R2006', '18-Feb-2017', '25-Mar-2017', 2420, 2605, 'Third Party Fire & Theft', 'c3014', 'L1298');
+Insert into RAGREEMENT values ('R2007', '29-Mar-2017', '04-Apr-2017', 1082, 2400, 'Comprehensive', 'c3015', 'L1298');
+Insert into RAGREEMENT values ('R2001', '18-Apr-2017', '25-Apr-2017', 2420, 2605, 'Third Party Fire & Theft', 'c3003', 'L1259');
+Insert into RAGREEMENT values ('R2002', '29-Mar-2017', '04-Apr-2017', 2605, 4761, 'Comprehensive', 'c3004', 'L1298');
+Insert into RAGREEMENT values ('R2008', '28-Mar-2017', '08-Apr-2017', 1041, 1968, 'Third Party Fire & Theft', 'c3011', 'L1259');
+Insert into RAGREEMENT values ('R2009', '11-Jan-2017', '07-Mar-2017', 9374, 10437, 'Comprehensive', 'c3002', 'L1298');
+Insert into RAGREEMENT values ('R2010', '07-Mar-2017', '02-Apr-2017', 8740, 9533, 'Third Party Property Damage', 'c3003', 'L1259');
+Insert into RAGREEMENT values ('R3011', '16-Feb-2017', '26-Mar-2017', 6158, 7324, 'Comprehensive', 'c3012', 'L1318');
+Insert into RAGREEMENT values ('R3012', '26-Feb-2017', '08-Mar-2017', 428, 1247, 'Third Party Property Damage', 'c3004', 'L1389');
+Insert into RAGREEMENT values ('R4013', '27-Mar-2017', '10-Mar-2017', 3880, 4506, 'Third Party Fire & Theft', 'c3003', 'L1485');
+Insert into RAGREEMENT values ('R4014', '30-Mar-2017', '15-Mar-2017', 3552, 4175, 'Comprehensive', 'c3010', 'L1472');
+Insert into RAGREEMENT values ('R4015', '18-Mar-2017', '22-Feb-2017', 1473, 1632, 'Third Party Property Damage', 'c3012', 'L1408');
+Insert into RAGREEMENT values ('R4016', '07-Mar-2017', '10-Mar-2017', 1634, 2000, 'Third Party Property Damage', 'c3001', 'L1408');
+Insert into RAGREEMENT values ('R4017', '28-Mar-2017', '01-Apr-2017', 4507, 4800, 'Third Party Fire & Theft', 'c3008', 'L1485');
+Insert into RAGREEMENT values ('R4018', '03-Apr-2017', '07-Apr-2017', 2001, 2221, 'Third Party Fire & Theft', 'c3007', 'L1408');
 
 -- insert fault report
 
@@ -87,6 +93,6 @@ Insert into FAULTREPORT values ('RP1001', '14-Mar-2017', '...', '141', 'L1156', 
 Insert into FAULTREPORT values ('RP1002', '12-Mar-2017', '...', '142', 'L1147', 'R1003');
 Insert into FAULTREPORT values ('RP2003', '26-Mar-2017', '...', '243', 'L1298', 'R2006');
 Insert into FAULTREPORT values ('RP2004', '10-Apr-2017', '...', '243', 'L1259', 'R2008');
-Insert into FAULTREPORT values ('RP3005', '15-Apr-2017', '...', '342', 'L1318', 'R3010');
+Insert into FAULTREPORT values ('RP3005', '15-Apr-2017', '...', '342', 'L1318', 'R2010');
 Insert into FAULTREPORT values ('RP3006', '01-Apr-2017', '...', '342', 'L1318', 'R3011');
 Insert into FAULTREPORT values ('RP3007', '12-Mar-2017', '...', '342', 'L1389', 'R3012');
